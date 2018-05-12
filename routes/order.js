cef833e895e4f6e44b975cf1809ceef1224a2d5f
Saad Zafar <31594740+saadie123@users.noreply.github.com>
2018-05-12 12:29:47 +0500

    Added stripe payment handleing

--- routes/order.js
+++ routes/order.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const stripe = require('stripe')('sk_test_JfMI20QjvPthFLqKdGJRZyXw');
 const Gig = require('../models/gig');
 
 const fee = 3.15;
@@ -12,4 +13,30 @@ router.get('/checkout/single-package/:id',(req,res) =>{
     });
 });
 
+router.get('/payment',(req, res) =>{
+    res.render('checkout/payment');
+});
+router.post('/payment',(req, res)=>{
+    const gig = req.session.gig;
+    let price = req.session.price;
+    price *= 100;
+    stripe.customers.create({
+        email: req.user.email
+    }).then(function(customer){
+        return stripe.customers.createSource(customer.id, {
+            source: req.body.stripeToken
+        });
+    }).then(function(source) {
+        return stripe.charges.create({
+            amount: price,
+            currency: 'usd',
+            customer: source.customer
+        });
+    }).then(function(charge) {
+        res.redirect('/');
+        // New charge created on a new customer
+    }).catch(function(err) {
+        // Deal with an error
+    });
+});
 module.exports = router;
